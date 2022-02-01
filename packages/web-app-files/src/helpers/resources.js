7ba6fb4d9c549afed81596cdfeb5e03640a4c4fc
Jannik Stehle <jannik.stehle@gmail.com>
2022-02-01 15:23:08 +0100

    Fix integration tests

--- packages/web-app-files/src/helpers/resources.js
+++ packages/web-app-files/src/helpers/resources.js
@@ -214,6 +214,7 @@ export function buildSharedResource(share, incomingShares = false, allowSharePer
     resource.status = share.state
     resource.name = path.basename(share.file_target)
     resource.path = share.file_target
+    resource.webDavPath = share.file_target
     resource.canDownload = () => share.state === ShareStatus.accepted
     resource.canShare = () => SharePermissions.share.enabled(share.permissions)
     resource.canRename = () => SharePermissions.update.enabled(share.permissions)
@@ -224,6 +225,7 @@ export function buildSharedResource(share, incomingShares = false, allowSharePer
     resource.shareOwnerDisplayname = share.displayname_owner
     resource.name = path.basename(share.path)
     resource.path = share.path
+    resource.webDavPath = share.path
     resource.canDownload = () => true
     resource.canShare = () => true
     resource.canRename = () => true
